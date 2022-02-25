c78aaade7b4fd0a9b93ab91e494289f5eb54138d
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-02-25 17:30:32 -0500

    finished

--- api/server.test.js
+++ api/server.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest')
 const server = require('./server')
 const db = require('../data/dbConfig')
+const Users = require('./auth/auth-model')
 
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
 
 beforeAll(async () => {
@@ -14,4 +15,34 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await db.destroy()
+})
+
+describe('POST /api/auth/register', () => {
+  test('returns an error if register is invalid ',  async () => {
+    const res = await request(server).post('/api/auth/register')
+    expect(res.body.message).toMatch(/username and password required/i)
+    await request(server).post('/api/auth/register').send({username: 'coco', password:'12345'})
+    const newUser = await request(server).post('/api/auth/register').send({username: 'coco', password:'12345'})
+    expect(newUser.body.message).toMatch(/username taken/i)
+  })
+  test('returns an username if successfully registered', async () => {
+    const res = await request(server).post('/api/auth/register').send({username: 'quinn', password:'12345'})
+    expect(res.body.username).toBe('quinn')
+    expect(res.body).toBeInstanceOf(Object)
+  })
+})
+
+describe('POST /api/auth/login', () => {
+  test('returns an error if username/password is missing or invalid', async () => {
+    const res = await request(server).post('/api/auth/login')
+    expect(res.body.message).toMatch(/username and password required/i)
+    await request(server).post('/api/auth/register').send({username: 'coco', password: '12345'})
+    let user = await request(server).post('/api/auth/login').send({username: 'coco', password: '1234'})
+    expect(user.body.message).toMatch(/invalid credentials/i)
+  })
+  test('returns welcome message if successfully logged in', async () => {
+    await request(server).post('/api/auth/register').send({username: 'coco', password: '12345'})
+    const res = await request(server).post('/api/auth/login').send({username: 'coco', password: '12345'})
+    expect(res.body.message).toMatch(`Welcome, coco`)
+  })
 })
